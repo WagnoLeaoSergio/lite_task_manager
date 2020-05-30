6fab004d1bd7a07d5e02f02762cdd22d163fa8d6
WagnoLeaoSergio <wagnoleao@gmail.com>
2020-05-30 14:33:54 -0300

    Updates

---
+++ precache-manifest.b297402c183ca332e73c660584e16e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcea1054f67ff30077333cdc5accff1a",
+    "url": "/lite_task_manager/index.html"
+  },
+  {
+    "revision": "682822fe71639e5235e2",
+    "url": "/lite_task_manager/static/css/main.2a4daeaf.chunk.css"
+  },
+  {
+    "revision": "7ae9d1f9b92a1ff4db99",
+    "url": "/lite_task_manager/static/js/2.5adca91a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lite_task_manager/static/js/2.5adca91a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682822fe71639e5235e2",
+    "url": "/lite_task_manager/static/js/main.8ba26fdc.chunk.js"
+  },
+  {
+    "revision": "474b35b9948a4c075000",
+    "url": "/lite_task_manager/static/js/runtime-main.212974a8.js"
+  }
+]);
